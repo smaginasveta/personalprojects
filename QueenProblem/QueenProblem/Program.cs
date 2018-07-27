96c43e4bea98c1d7c9bf29a5e49b3be09adcd23f
Svitlana Smahina <smaginasveta@gmail.com>
2018-07-27 13:57:30 +0300

    initial commit for QueenProblem solution

---
+++ QueenProblem/QueenProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*The N Queen is the problem of placing N chess queens on an N×N chessboard so that no two queens attack each other.*/
+
+namespace QueenProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
